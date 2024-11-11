869ed104e2b4d8b0351f9ab03031663569cdbe35
MrV09 <vladcirlugea@gmail.com>
2024-11-11 13:59:47 +0200

    Added main page with mock info Added 7-day history page Added navigation from main page to 7-day history

---
+++ weatherapp/lib/pages/seven_day_history.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SevenDayHistory extends StatelessWidget {
+  const SevenDayHistory({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('7 Day History'),
+      ),
+      body: Center(
+        child: const Text('7 Day History'),
+      ),
+    );
+  }
+}
