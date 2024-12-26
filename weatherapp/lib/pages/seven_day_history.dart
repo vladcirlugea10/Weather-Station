07f157b259e24f1e9325c42e723ffa32d362cb6f
MrV09 <vladcirlugea@gmail.com>
2024-12-26 12:49:18 +0200

    Merge

--- weatherapp/lib/pages/seven_day_history.dart
+++ weatherapp/lib/pages/seven_day_history.dart
@@ -1,12 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-
-class SevenDayHistory extends StatelessWidget {
-  const SevenDayHistory({super.key});
-
-  @override
-  Widget build(BuildContext context){
-=======
 import 'package:weatherapp/widgets/DayCard.dart';
 import 'package:firebase_database/firebase_database.dart';
 
@@ -83,28 +75,10 @@ class _SevenDayHistoryState extends State<SevenDayHistory> {
 
   @override
   Widget build(BuildContext context) {
->>>>>>> 7DayHistory
     return Scaffold(
       appBar: AppBar(
         title: const Text('7 Day History'),
       ),
-<<<<<<< HEAD
-      body: const Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: const [
-              Text(
-                'Timisoara',
-              ),
-            ],
-          ),
-        )
-    );
-  }
-}
-=======
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: isLoading
@@ -126,4 +100,3 @@ class _SevenDayHistoryState extends State<SevenDayHistory> {
     );
   }
 }
->>>>>>> 7DayHistory
